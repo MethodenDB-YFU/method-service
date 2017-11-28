087dee2ea508e9233720d85296e93e9d80d4798f
Alex Senger <alex@alexmsenger.de>
2017-11-28 17:27:42 +0100

    #10 clean up deployment scripts

--- deploy/app/up.sh
+++ deploy/app/up.sh
@@ -2,4 +2,4 @@
 latest=`cat deploy/app/.latest`
 
 printf '[INFO] Starting Shop App Version %s\n' "$latest"
-docker run --rm -it --name methodendb-app-$latest -p8082:8080 --net=ac-net --net-alias=methodendb-app --env ENVIRONMENT="docker" yfudeutschland/methodendb-app:$latest
+docker run --rm -it --name methodendb-app-$latest -p8082:8080 --env ENVIRONMENT="docker" yfudeutschland/methodendb-app:$latest
--- deploy/db/up.sh
+++ deploy/db/up.sh
@@ -2,4 +2,5 @@
 latest=`cat deploy/db/.latest`
 
 printf "[INFO] Starting up DB version %s\n" "$latest"
-docker run -it -p5432:5432 --rm --name methodendb-db-$latest --net=ac-net --net-alias=shop-db yfudeutschland/methodendb-db:$latest
+# docker run -it -p5432:5432 --rm --name methodendb-db-$latest yfudeutschland/methodendb-db:$latest
+docker run -d -p5432:5432 --rm --name methodendb-db-$latest yfudeutschland/methodendb-db:$latest
