368ee89c3d1b7de6a0cff4b600d4be1e048863b7
Alex Senger <alex@alexmsenger.de>
2017-11-28 17:31:15 +0100

    #10 cleanup deployment scripts

--- deploy/app/up.sh
+++ deploy/app/up.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 latest=`cat deploy/app/.latest`
 
-printf '[INFO] Starting Shop App Version %s\n' "$latest"
+printf '[INFO] Starting App Version %s\n' "$latest"
 docker run --rm -it --name methodendb-app-$latest -p8082:8080 --env ENVIRONMENT="docker" yfudeutschland/methodendb-app:$latest
